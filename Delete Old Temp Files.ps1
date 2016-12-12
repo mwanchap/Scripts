40972fc47927db3a774b386a0010049c06d222d3
Matt Wanchap <matt.wanchap@cpal.com.au>
2016-12-12 16:09:12 +1000

    Updated Delete Old Temp Files.ps1 script

---
+++ Delete Old Temp Files.ps1
@@ -0,0 +1,9 @@
+﻿$tempDir = "C:\temp\*"
+
+#get a list of items that haven't been touched since before the "cutoff date"
+$cutoffDate = [DateTime]::Today.AddYears(-1)
+$items = Get-ChildItem $tempDir
+$oldItems = $items | Where-Object { $_.LastAccessTime -le $cutoffDate }
+
+#delete them
+Remove-Item $oldItems -Force -Recurse
